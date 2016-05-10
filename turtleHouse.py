9c9e0077bda4d3be47c3d731fd14102198a6f61a
Cherry <cmulliss@gmail.com>
2016-05-10 19:25:21 +0100

    Turtle in a house with colour and resized turtle

---
+++ turtleHouse.py
@@ -0,0 +1,21 @@
+from turtle import *
+
+mode('logo')
+shape('turtle')
+speed(5)
+color('red', 'blue')
+for i in range (4):
+    fd(200)
+    rt(90)
+penup()
+goto(0,200)
+pendown()
+rt(60)
+goto(100,300)
+rt(60)
+goto(200,200)
+penup()
+shapesize(5,5)
+goto(100,100)
+rt(240)
+width(200)
---
+++ turtleSquareWithDiagonals.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+mode('logo')
+shape('turtle')
+speed(5)
+color('red', 'blue')
+for i in range (4):
+    fd(200)
+    rt(90)
+  
+goto(200,200)
+goto(0,200)
+goto(200,0)
+rt(720)
