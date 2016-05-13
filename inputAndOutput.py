a8fd2cd6c6998f708f64549f222b5af2e2353214
Cherry Mulliss <cmulliss@gmail.com>
2016-05-13 19:25:00 +0100

    input and output

---
+++ inputAndOutput.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+myName = input('Please enter your name: ')
+myEmail = input('Please enter your email address: ')
+print('My name is {} and my email address is {}'.format(myName, myEmail))
+# or
+print('My name is %s and my email address is %s'%(myName, myEmail))
