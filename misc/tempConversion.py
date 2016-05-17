355e8ba3f43f9e03708a024424524775292bac3f
Cherry Mulliss <cmulliss@gmail.com>
2016-05-17 16:25:07 +0100

    Created a program to convert deg C to deg F

---
+++ misc/tempConversion.py
@@ -0,0 +1,12 @@
+# program to convert degrees f to degrees c
+# need to use (degF - 32) * 5/9
+
+degF = int(input('Enter a temperature in degrees F: '))
+degC = (degF -32) * 5/9
+print(degC)
+
+
+
+
+
+           
