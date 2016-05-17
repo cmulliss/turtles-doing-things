c921c39836b70259e5e6648f1a4ed3d5293c9fce
Cherry <cmulliss@gmail.com>
2016-05-17 21:12:54 +0100

    Improved temp program

--- misc/tempConversion.py
+++ misc/tempConversion.py
@@ -1,9 +1,10 @@
 # program to convert degrees f to degrees c
 # need to use (degF - 32) * 5/9
-
+user = input('Hello, what is your name? ')
+print('Hello', user)
 degF = int(input('Enter a temperature in degrees F: '))
 degC = (degF -32) * 5/9
-print(degC)
+print(degF ,'degrees F converts to ', float(degC) ,'degrees C')
 
 
 
