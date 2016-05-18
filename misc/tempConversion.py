14902ecf8c43c8bf0e722c6a362e37f9d60e59c1
Cherry Mulliss <cmulliss@gmail.com>
2016-05-18 08:37:34 +0100

    TempConversion trying some additional things

--- misc/tempConversion.py
+++ misc/tempConversion.py
@@ -1,12 +1,25 @@
-# program to convert degrees f to degrees c
+# program to convert degrees F to degrees C
 # need to use (degF - 32) * 5/9
+# or for degrees C to degrees F
+# (degC * 9/5) + 32
 
-degF = int(input('Enter a temperature in degrees F: '))
-degC = (degF -32) * 5/9
-print(degC)
-
-
-
+convertCtoF = (degC * 9/5) + 32
+convertFtoC = (degF -32) * 5/9
 
 
-           
+print('Choose ({})  or ({})'.format ('convertFtoC', 'convertCtoF'))
+
+if convert = convertFtoC:
+    input('Enter the temp in degrees F: ')
+        return(
+
+choice = input('Choose (convertFtoC) or (convertCtoF): ')
+
+if choice == convertCtoF:
+    print(convertCtoF)
+elif choice == convertFtoC:
+        print(convertFtoC)
+else:
+    print('Not sure what you have chosen, try again!')
+
+
